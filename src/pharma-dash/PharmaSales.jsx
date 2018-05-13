c6e601db24e45548c02f5be4e93063e613234030
Jacob Maarse <jacob.maarse@gmail.com>
2018-05-13 15:14:49 -0400

    added heatmap and pie chart

---
+++ src/pharma-dash/PharmaSales.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import RaisedButton from 'material-ui/RaisedButton';
+import ReactChartkick, { GeoChart, LineChart } from 'react-chartkick';
+// import Chart from 'chart.js';
+
+// ReactChartkick.addAdapter(Chart)
+
+class PharmaSales extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    render () {
+
+        return (
+            <div id="heatmap">
+                <GeoChart data={[["United States",44],["Germany",23],["Brazil",22],["Canada",21],["China",13], ["India",70]]} colors={[ "#a1d9ce", "#69c2b0" , "#3da18d" , "#2f7d6d", "#22594e"]}/>
+            </div>
+        )
+    }
+}
+
+export default PharmaSales;
